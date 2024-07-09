c118ba00888e830e525edc6442483f928fde45c4
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-09 14:47:05 +0100

    0x01 basic_auth pep8 validation_2

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,7 +9,8 @@ from typing import TypeVar
 class BasicAuth(Auth):
     """ BasicAuth class """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """ returns the Base64 part of the Authorization
         header for a Basic Authentication """
         if authorization_header and \
@@ -18,26 +19,31 @@ class BasicAuth(Auth):
             return authorization_header[len('Basic '):]
         return None
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """ returns the decoded value of a Base64
         string base64_authorization_header """
         if base64_authorization_header and \
                 isinstance(base64_authorization_header, str):
             try:
-                return base64.b64decode(base64_authorization_header).decode('utf-8')
-            except:
+                b64 = base64.b64decode(base64_authorization_header)
+                return b64.decode('utf-8')
+            except Exception:
                 return None
         return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
-        """ returns the user email and password from the Base64 decoded value """
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ returns the user email and password
+        from the Base64 decoded value """
         if decoded_base64_authorization_header and \
                 isinstance(decoded_base64_authorization_header, str) and \
                 ':' in decoded_base64_authorization_header:
             return tuple(decoded_base64_authorization_header.split(':', 1))
         return (None, None)
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """ returns the User instance based on his email and password """
         if user_email and isinstance(user_email, str) and \
                 user_pwd and isinstance(user_pwd, str):
