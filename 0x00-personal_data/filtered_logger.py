4e234d0c402fc5c747884afc7e049b2e89d9d9de
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-13 10:51:34 +0100

    0x00 get_db corr5

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,7 +56,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     user = getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    database = getenv('PERSONAL_DATA_DB_NAME')
+    database = getenv('PERSONAL_DATA_DB_NAME', '')
     db_cnx = mysql.connector.connection.MySQLConnection(user=user,
                                                         password=password,
                                                         host=host,
