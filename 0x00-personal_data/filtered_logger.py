84d3a9ad6bab97df84bf56e6ed7af7acb8347433
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-13 10:14:01 +0100

    0x00 get_db corr2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,7 +53,7 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ returns a connector to the database """
-    return mysql.connector.connect(
+    return mysql.connector.connection.MySQLConnection(
         user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
