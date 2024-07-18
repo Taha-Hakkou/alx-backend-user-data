4400f4230ca0f0e71fa4950f6c8cb539d45be080
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-18 18:15:54 +0100

    0x03 app.py corr

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/", methods=['GET'], strict_slashes=False)
 def index():
     """Index route that returns a JSON payload
     """
-    return jsonify({"message": "Bienvenue"})
+    return jsonify({"message": "Bienvenue"}), 200
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
